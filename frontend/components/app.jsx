e48d341342a0b3cd737c8711017ccde49a77482b
darryllittman <darryl.littman@gmail.com>
2016-08-30 15:53:43 -0700

    Guest Login Login and Create an Account buttons created

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Greeting from './greeting';
 
 const App = ({children}) => (
   <div>
     <h1>Logo goes here</h1>
+    <Greeting />
     {children}
   </div>
 );
--- frontend/components/greeting.jsx
+++ frontend/components/greeting.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const Login = () => (
-  <button>Login</button>
+const Greeting = () => (
+  <div>
+    <button to="/home">Guest Login</button>
+    <button to="/login">Login</button>
+    <button to="/signup">Create an Account</button>
+  </div>
 );
+
+export default Greeting;
