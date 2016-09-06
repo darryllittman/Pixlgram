fa8b84a568df29a3ff407a25b7f8d8be1960f0ec
darryllittman <darryl.littman@gmail.com>
2016-09-06 14:50:59 -0700

    fix photo show. fix routing from photos/add to photo show

--- frontend/components/photo.jsx
+++ frontend/components/photo.jsx
@@ -1,78 +1,40 @@
-// import React from 'react';
-// import { withRouter } from 'react-router';
-
-
-// class Photo {
-//   constructor (props){
-//     super(props);
-//     this.url = "";
-//   }
-//
-//   componentDidMount() {
-//     this.url = this.props.photo.url;
-//   }
-//
-//   render() {
-//     return(
-//       <div>
-//         <img src={this.url} />
-//
-//         <header className="photo-header">
-//           <h4>username here</h4>
-//         </header>
-//
-//         <ul className="comment-list">
-//           <li>likes list here</li>
-//           <li>comment</li>
-//           <li>comment</li>
-//           <li>comment</li>
-//         </ul>
-//
-//         <textarea className="add-comment"></textarea>
-//       </div>
-//     );
-//   }
-// }
-//   //src = props.photo.url
-//
-//
-//   // debugger
-//   // src={props.photo.url}
-//
-//
-// export default withRouter(Photo);
-
-
-
-
 import React from 'react';
 import { withRouter } from 'react-router';
 
 
-const Photo = (props) => {
-  //src = props.photo.url
+class Photo extends React.Component {
+  constructor (props){
+    super(props);
+  }
 
+  componentDidMount() {
+    this.photos = this.props.requestAllPhotos();
+    this.id = this.props.location.pathname.slice(8);
+  }
 
-  // debugger
-  // src={props.photo.url}
-  return(
-  <div>
-    <img src="" />
-
-    <header className="photo-header">
-      <h4>username here</h4>
-    </header>
-
-    <ul className="comment-list">
-      <li>likes list here</li>
-      <li>comment</li>
-      <li>comment</li>
-      <li>comment</li>
-    </ul>
-
-    <textarea className="add-comment"></textarea>
-  </div>
-);};
-
+  render() {
+    if (this.props.photos[this.id]) {
+      return (
+        <ul>
+          <li>
+            <img src={this.props.photos[this.id].url} />
+          </li>
+          <li>
+            {this.props.photos[this.id].caption}
+          </li>
+          <li>
+            {this.props.photos[this.id].location}
+          </li>
+        </ul>
+      );
+    } else {
+      return (
+        <div>
+          loading...
+        </div>
+      );
+    }
+  }
+}
 
 export default withRouter(Photo);
