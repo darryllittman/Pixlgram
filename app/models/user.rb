9eabe77549f29e9377cc2d166eb6e1ca86e90d2e
darryllittman <darryl.littman@gmail.com>
2016-09-07 08:49:58 -0700

    comment model and migration with associations working

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   validates :password, length: {minimum: 6, allow_nil: true}
 
   has_many :photos
+  has_many :comments
 
   after_initialize :ensure_session_token
   before_validation :ensure_session_token_uniqueness
